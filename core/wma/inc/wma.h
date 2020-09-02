f94b0073a225085c721f436525e5bb18294d18d7
Will Huang <wilhuang@codeaurora.org>
2020-09-02 16:31:30 +0800

    qcacld-3.0: Put install key after associate complete for FT roam
    If FT roam with LFR2.0 enabled, pre-auth/reassociate happen in host,
    PTK will be installed early after pre-auth negotiation, so need put it
    after association complete, but currently we only save PTK key material
    and don't install it.
    
    Because crypto was converged and original eWNI_SME_FT_UPDATE_KEY has
    been removed, move delayed installing key after association complete
    notified at hdd layer, rather than in function wma_add_sta_req_sta_mode
    at wma layer. And remove useless code related with original FT key
    installing.
    
    Change-Id: I3bc52b65d17342edab755f7904d743c3433f99ce
    CRs-Fixed: 2768307

--- core/wma/inc/wma.h
+++ core/wma/inc/wma.h
@@ -683,7 +683,6 @@ struct wma_invalid_peer_params {
  * @is_channel_switch: is channel switch
  * @pause_bitmap: pause bitmap
  * @nwType: network type (802.11a/b/g/n/ac)
- * @staKeyParams: sta key parameters
  * @ps_enabled: is powersave enable/disable
  * @peer_count: peer count
  * @roam_synch_in_progress: flag is in progress or not
@@ -733,7 +732,6 @@ struct wma_txrx_node {
 	uint8_t nss;
 	uint16_t pause_bitmap;
 	uint32_t nwType;
-	tSetStaKeyParams *staKeyParams;
 	uint32_t peer_count;
 	bool roam_synch_in_progress;
 	void *plink_status_req;
