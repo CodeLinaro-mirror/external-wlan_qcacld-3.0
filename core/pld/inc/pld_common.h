0c5bbcf95760f57fce8e4a7d106d0e64687dc45e
Srinivas Girigowda <sgirigow@codeaurora.org>
2020-10-22 09:54:27 -0700

    Revert "qcacld-3.0: Add support for PCIE gen switch"
    This reverts Change-Id: Ie1a3273fa8949131937f43d3551cb500a033d59d
    
    Change-Id: Ica8a1fcc0a5fce17464d5fa520868e9e5b278530

--- core/pld/inc/pld_common.h
+++ core/pld/inc/pld_common.h
@@ -448,18 +448,6 @@ int pld_get_fw_files_for_target(struct device *dev,
 				u32 target_type, u32 target_version);
 int pld_prevent_l1(struct device *dev);
 void pld_allow_l1(struct device *dev);
-
-/**
- * pld_set_pcie_gen_speed() - Set PCIE gen speed
- * @dev: device
- * @pcie_gen_speed: Required PCIE gen speed
- *
- * Send required PCIE Gen speed to platform driver
- *
- * Return: 0 for success. Negative error codes.
- */
-int pld_set_pcie_gen_speed(struct device *dev, u8 pcie_gen_speed);
-
 void pld_is_pci_link_down(struct device *dev);
 void pld_get_bus_reg_dump(struct device *dev, uint8_t *buf, uint32_t len);
 int pld_shadow_control(struct device *dev, bool enable);
