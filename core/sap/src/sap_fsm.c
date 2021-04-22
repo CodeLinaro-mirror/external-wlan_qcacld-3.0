f92c35963f547e75e8f5875f1bdc46d6095d3b3c
Liangwei Dong <liangwei@codeaurora.org>
2021-04-22 17:01:21 +0800

    qcacld-3.0: Fix DFS CAC start event missing
    At present check SAP on DFS is based on primary channel,
    it doesn't work for 160M case. Use "sap_operating_on_dfs"
    to do the check before deliver CAC Start event.
    
    Change-Id: I3896dc2738da16b9bff0d54826191d0edcc251a4
    CRs-Fixed: 2929729

--- core/sap/src/sap_fsm.c
+++ core/sap/src/sap_fsm.c
@@ -1913,12 +1913,10 @@ static QDF_STATUS sap_cac_start_notify(mac_handle_t mac_handle)
 	uint8_t intf = 0;
 	struct mac_context *mac = MAC_CONTEXT(mac_handle);
 	QDF_STATUS qdf_status = QDF_STATUS_E_FAILURE;
-	qdf_freq_t ch_freq;
 
 	for (intf = 0; intf < SAP_MAX_NUM_SESSION; intf++) {
 		struct sap_context *sap_context =
 			mac->sap.sapCtxList[intf].sap_context;
-		struct csr_roam_profile *profile;
 
 		if (((QDF_SAP_MODE == mac->sap.sapCtxList[intf].sapPersona)
 		    ||
@@ -1926,10 +1924,9 @@ static QDF_STATUS sap_cac_start_notify(mac_handle_t mac_handle)
 		    && mac->sap.sapCtxList[intf].sap_context &&
 		    (false == sap_context->isCacStartNotified)) {
 			/* Don't start CAC for non-dfs channel, its violation */
-			profile = &sap_context->csr_roamProfile;
-			ch_freq = profile->op_freq;
-			if (!wlan_reg_is_dfs_for_freq(mac->pdev,
-						      ch_freq))
+			if (!sap_operating_on_dfs(
+					mac,
+					mac->sap.sapCtxList[intf].sap_context))
 				continue;
 			sap_debug("sapdfs: Signaling eSAP_DFS_CAC_START to HDD for sapctx[%pK]",
 				  sap_context);
@@ -1995,7 +1992,6 @@ static QDF_STATUS sap_cac_end_notify(mac_handle_t mac_handle,
 	uint8_t intf;
 	struct mac_context *mac = MAC_CONTEXT(mac_handle);
 	QDF_STATUS qdf_status = QDF_STATUS_E_FAILURE;
-	uint32_t freq;
 
 	/*
 	 * eSAP_DFS_CHANNEL_CAC_END:
@@ -2006,7 +2002,6 @@ static QDF_STATUS sap_cac_end_notify(mac_handle_t mac_handle,
 	for (intf = 0; intf < SAP_MAX_NUM_SESSION; intf++) {
 		struct sap_context *sap_context =
 			mac->sap.sapCtxList[intf].sap_context;
-		struct csr_roam_profile *profile;
 
 		if (((QDF_SAP_MODE == mac->sap.sapCtxList[intf].sapPersona)
 		    ||
@@ -2016,12 +2011,9 @@ static QDF_STATUS sap_cac_end_notify(mac_handle_t mac_handle,
 		    sap_is_dfs_cac_wait_state(sap_context)) {
 			sap_context = mac->sap.sapCtxList[intf].sap_context;
 			/* Don't check CAC for non-dfs channel */
-			profile = &sap_context->csr_roamProfile;
-			freq = profile->op_freq;
-			if (CHANNEL_STATE_DFS !=
-			    wlan_reg_get_5g_bonded_channel_state_for_freq(mac->pdev,
-									  freq,
-									  profile->ch_params.ch_width))
+			if (!sap_operating_on_dfs(
+					mac,
+					mac->sap.sapCtxList[intf].sap_context))
 				continue;
 
 			/* If this is an end notification of a pre cac, the
