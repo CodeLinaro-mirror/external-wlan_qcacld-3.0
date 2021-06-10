4b163a44f203b0aa441bb4bebc249d39161fe084
Zhiwei Yang <zhiwyang@qti.qualcomm.com>
2021-06-10 16:58:22 -0700

    qcacld-3.0: Define A_OFFSETOF macro
    Define A_OFFSETOF macro to fix compilation
    errors.
    
    A_OFFSETOF macro is mapped to macro offsetof()
    defined in stddef.h.
    
    Change-Id: I766826d610fcb6a06775f07bcd8a041ee29be89b
    CRs-Fixed: 2967130

--- uapi/linux/osapi_linux.h
+++ uapi/linux/osapi_linux.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2013-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2018, 2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -73,6 +74,7 @@
 #define A_PRINTF(args ...) \
 	QDF_TRACE_ERROR(QDF_MODULE_ID_QDF, args)
 #define A_SNPRINTF(buf, len, args ...)   snprintf(buf, len, args)
+#define A_OFFSETOF(type, field)   offsetof(type, field)
 
 /*
  * Timer Functions
