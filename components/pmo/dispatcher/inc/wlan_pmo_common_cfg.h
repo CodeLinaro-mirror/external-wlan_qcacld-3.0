eqs-development/android_kernel_motorola_sm8475-modules

qcacld-3.0: Avoid fw crash in some wow case on third party
Due to the limitation on third party platform, add ini to take
special care of the below wow case to avoid fw crash.
The sap/p2p_go shall kick out all the connected sta/p2p_gc and
then go to suspend if d0wow/d3wow is not supported.
Although there is wakelock try to prevent suspend after tdls link
setup, it won't take effect because auto sleep mechanism is not
supported on some third party platform, so teardown tdls link
by force before suspend.

Change-Id: I14bfe32f04cfc34d4f55a09820e42e65a1d9f925
CRs-Fixed: 2935295

qcom/opensource/wlan/qcacld-3.0/components/pmo/dispatcher/inc/wlan_pmo_common_cfg.h
@@ -546,6 +546,29 @@
 		CFG_VALUE_OR_DEFAULT, \
 		"configure igmp offload support version")
 
+/*
+ * <ini>
+ * disconnect_sap_tdls_in_wow - disconnect sap tdls in wow
+ * @Min: 0
+ * @Max: 1
+ * @Default: 0
+ *
+ * Due to the limitation on third party platform, add ini to take
+ * special care of the below wow case to avoid fw crash.
+ * The sap/p2p_go shall kick out all the connected sta/p2p_gc and
+ * then go to suspend considering d0wow/d3wow is not supported.
+ * Teardown tdls link proactively since auto sleep mechanism not
+ * supported.
+ *
+ * Usage: External
+ *
+ * </ini>
+ */
+#define CFG_DISCONNECT_SAP_TDLS_IN_WOW CFG_INI_BOOL( \
+		"disconnect_sap_tdls_in_wow", \
+		0, \
+		"disconnect sap tdls in wow")
+
 #define CFG_PMO_COMMON_ALL \
 	CFG(CFG_ENABLE_SAP_SUSPEND) \
 	CFG(CFG_PMO_ENABLE_HOST_ARPOFFLOAD) \
@@ -568,6 +591,7 @@
 	CFG(CFG_PMO_MOD_DTIM_ON_SYS_SUSPEND) \
 	CFG(CFG_ENABLE_BUS_SUSPEND_IN_SAP_MODE) \
 	CFG(CFG_ENABLE_BUS_SUSPEND_IN_GO_MODE)\
-	CFG(CFG_IGMP_VERSION_SUPPORT)
+	CFG(CFG_IGMP_VERSION_SUPPORT) \
+	CFG(CFG_DISCONNECT_SAP_TDLS_IN_WOW)
 
 #endif /* WLAN_PMO_COMMON_CFG_H__ */

qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_power.c
@@ -2190,6 +2190,12 @@ static int __wlan_hdd_cfg80211_suspend_wlan(struct wiphy *wiphy,
 	struct wlan_objmgr_vdev *vdev;
 	int rc;
 	wlan_net_dev_ref_dbgid dbgid = NET_DEV_HOLD_CFG80211_SUSPEND_WLAN;
+	struct hdd_hostapd_state *hapd_state;
+	struct csr_del_sta_params params = {
+		.peerMacAddr = QDF_MAC_ADDR_BCAST_INIT,
+		.reason_code = REASON_DEAUTH_NETWORK_LEAVING,
+		.subtype = SIR_MAC_MGMT_DEAUTH,
+	};
 
 	hdd_enter();
 
@@ -2252,6 +2258,14 @@ static int __wlan_hdd_cfg80211_suspend_wlan(struct wiphy *wiphy,
 					hdd_adapter_dev_put_debug(next_adapter,
 								  dbgid);
 				return -EOPNOTSUPP;
+			} else if (ucfg_pmo_get_disconnect_sap_tdls_in_wow(
+				   hdd_ctx->psoc)) {
+				hapd_state =
+					WLAN_HDD_GET_HOSTAP_STATE_PTR(adapter);
+				if (hapd_state)
+					hdd_softap_deauth_all_sta(adapter,
+								  hapd_state,
+								  &params);
 			}
 		} else if (QDF_P2P_GO_MODE == adapter->device_mode) {
 			if (!ucfg_pmo_get_enable_sap_suspend(
@@ -2265,7 +2279,19 @@ static int __wlan_hdd_cfg80211_suspend_wlan(struct wiphy *wiphy,
 					hdd_adapter_dev_put_debug(next_adapter,
 								  dbgid);
 				return -EOPNOTSUPP;
+			} else if (ucfg_pmo_get_disconnect_sap_tdls_in_wow(
+				   hdd_ctx->psoc)) {
+				hapd_state =
+					WLAN_HDD_GET_HOSTAP_STATE_PTR(adapter);
+				if (hapd_state)
+					hdd_softap_deauth_all_sta(adapter,
+								  hapd_state,
+								  &params);
 			}
+		} else if (QDF_TDLS_MODE == adapter->device_mode) {
+			if (ucfg_pmo_get_disconnect_sap_tdls_in_wow(
+								hdd_ctx->psoc))
+				ucfg_tdls_teardown_links_sync(hdd_ctx->psoc);
 		}
 		hdd_adapter_dev_put_debug(adapter, dbgid);
 	}

CodeLinaro-mirror/external-wlan_qcacld-3.0

components/pmo/dispatcher/inc/wlan_pmo_common_cfg.h
@@ -416,6 +416,29 @@
 	CFG_VALUE_OR_DEFAULT, \
 	"RA rate limit interval")
 
+/*
+ * <ini>
+ * disconnect_sap_tdls_in_wow - disconnect sap tdls in wow
+ * @Min: 0
+ * @Max: 1
+ * @Default: 0
+ *
+ * Due to the limitation on third party platform, add ini to take
+ * special care of the below wow case to avoid fw crash.
+ * The sap/p2p_go shall kick out all the connected sta/p2p_gc and
+ * then go to suspend considering d0wow/d3wow is not supported.
+ * Teardown tdls link proactively since auto sleep mechanism not
+ * supported.
+ *
+ * Usage: External
+ *
+ * </ini>
+ */
+#define CFG_DISCONNECT_SAP_TDLS_IN_WOW CFG_INI_BOOL( \
+		"disconnect_sap_tdls_in_wow", \
+		0, \
+		"disconnect sap tdls in wow")
+
 #define CFG_PMO_COMMON_ALL \
 	CFG(CFG_ENABLE_SAP_SUSPEND) \
 	CFG(CFG_PMO_ENABLE_HOST_ARPOFFLOAD) \
@@ -433,6 +456,7 @@
 	CFG(CFG_PMO_ACTIVE_MODE) \
 	CFG(CFG_PMO_PWR_FAILURE) \
 	CFG(CFG_PMO_WOW_DATA_INACTIVITY_TIMEOUT) \
-	CFG(CFG_RA_RATE_LIMIT_INTERVAL)
+	CFG(CFG_RA_RATE_LIMIT_INTERVAL) \
+	CFG(CFG_DISCONNECT_SAP_TDLS_IN_WOW)
 
 #endif /* WLAN_PMO_COMMON_CFG_H__ */

core/hdd/src/wlan_hdd_power.c
@@ -1834,6 +1834,12 @@ static int __wlan_hdd_cfg80211_suspend_wlan(struct wiphy *wiphy,
 	struct hdd_adapter *adapter;
 	mac_handle_t mac_handle;
 	int rc;
+	struct hdd_hostapd_state *hapd_state;
+	struct csr_del_sta_params params = {
+		.peerMacAddr = QDF_MAC_ADDR_BCAST_INIT,
+		.reason_code = eSIR_MAC_DEAUTH_LEAVING_BSS_REASON,
+		.subtype = SIR_MAC_MGMT_DEAUTH,
+	};
 
 	hdd_enter();
 
@@ -1885,6 +1891,14 @@ static int __wlan_hdd_cfg80211_suspend_wlan(struct wiphy *wiphy,
 				 */
 				hdd_err("SAP does not support suspend!!");
 				return -EOPNOTSUPP;
+			} else if (ucfg_pmo_get_disconnect_sap_tdls_in_wow(
+				   hdd_ctx->psoc)) {
+				hapd_state =
+					WLAN_HDD_GET_HOSTAP_STATE_PTR(adapter);
+				if (hapd_state)
+					hdd_softap_deauth_all_sta(adapter,
+								  hapd_state,
+								  &params);
 			}
 		} else if (QDF_P2P_GO_MODE == adapter->device_mode) {
 			if (!ucfg_pmo_get_enable_sap_suspend(
@@ -1894,7 +1908,19 @@ static int __wlan_hdd_cfg80211_suspend_wlan(struct wiphy *wiphy,
 				 */
 				hdd_err("GO does not support suspend!!");
 				return -EOPNOTSUPP;
+			} else if (ucfg_pmo_get_disconnect_sap_tdls_in_wow(
+				   hdd_ctx->psoc)) {
+				hapd_state =
+					WLAN_HDD_GET_HOSTAP_STATE_PTR(adapter);
+				if (hapd_state)
+					hdd_softap_deauth_all_sta(adapter,
+								  hapd_state,
+								  &params);
 			}
+		} else if (QDF_TDLS_MODE == adapter->device_mode) {
+			if (ucfg_pmo_get_disconnect_sap_tdls_in_wow(
+								hdd_ctx->psoc))
+				ucfg_tdls_teardown_links(hdd_ctx->psoc);
 		}
 	}
 	/* p2p cleanup task based on scheduler */