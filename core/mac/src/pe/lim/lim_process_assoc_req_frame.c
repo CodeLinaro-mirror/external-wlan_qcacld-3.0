c91344ee7f03ed49ce3d7592ad3c3115d257019a
bings <quic_bings@quicinc.com>
2021-09-14 21:47:56 +0530

    qcacld-3.0: Update HE connection width correctly
    Currently driver does not update HE connection width
    correctly when it indicates about assoc indication from
    lim to hdd. Which results in incorrect indication of chwidth
    to hdd.
    
    To address this issue update HE connection width correctly
    from lim to hdd
    
    Change-Id: I9ccc4435dc621ed3698f944fd2821afd648f2d1a
    CRs-Fixed: 3028799

--- core/mac/src/pe/lim/lim_process_assoc_req_frame.c
+++ core/mac/src/pe/lim/lim_process_assoc_req_frame.c
@@ -2781,6 +2781,17 @@ lim_convert_channel_width_enum(enum phy_ch_width ch_width)
 	return eHT_CHANNEL_WIDTH_20MHZ;
 }
 
+static void lim_fill_assoc_ind_he_bw_info(tpLimMlmAssocInd assoc_ind,
+					  tpDphHashNode sta_ds,
+					  struct pe_session *session_entry)
+{
+	if (lim_is_sta_he_capable(sta_ds) &&
+	    lim_is_session_he_capable(session_entry)) {
+		assoc_ind->ch_width =
+			lim_convert_channel_width_enum(sta_ds->ch_width);
+	}
+}
+
 /**
  * lim_fill_assoc_ind_real_max_mcs_idx() - fill max real mcs index to assoc ind
  * @assoc_ind: assoc_ind to fill
@@ -3060,9 +3071,7 @@ bool lim_fill_lim_assoc_ind_params(
 	assoc_ind->is_sae_authenticated =
 				assoc_req->is_sae_authenticated;
 	/* updates HE bandwidth in assoc indication */
-	if (wlan_reg_is_6ghz_chan_freq(session_entry->curr_op_freq))
-		assoc_ind->ch_width =
-			lim_convert_channel_width_enum(sta_ds->ch_width);
+	lim_fill_assoc_ind_he_bw_info(assoc_ind, sta_ds, session_entry);
 	lim_fill_assoc_ind_real_max_mcs_idx(assoc_ind, assoc_req,
 					    sta_ds, session_entry);
 
