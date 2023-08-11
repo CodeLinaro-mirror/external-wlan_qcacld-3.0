arter97/caf_qcacld-3.0

qcacld-3.0: Synchronize country set driver command and idle shutdown
Currently there is no synchronization between country set command
and idle shutdown which leads to undefined behavior in the host
driver.

In the current issue, host driver gets the country change command
and it sends the command to FW, by the time FW sends the response
of the country change command, host driver triggers the idle
shutdown and idle shutdown starts the psoc transition,
in the path to process the regulatory event, host driver tries to
start the psoc operation in hdd country change work handle. Since,
hdd transition is in progress, hdd work handle does not get the
psoc operation and driver reschedules the work until it gets the
psoc operstion. Once the idle shutdown is complete, hdd country
change work handle get the psoc operation object, since idle
shutdown changes the memory domain from active to init domain
and psoc operation gets allocated in init domain. Now by the time
hdd country change work handle executes, host driver got the
interface up and as part of the hdd_open host driver changes the
memory domain from init to active domain. Now, when the hdd country
change work completes, it tries to release the memory of psoc operation,
this gives the issue of incorrect memory domain on mem free because
memory was allocated in init domain and getting released in active
domain.

To fix this issue, take the psoc operation in the host driver when it
receives the country change command so that when idle shutdown is
triggered, it waits for this operation to complete before proceeding
with the psoc transition.
Also, add wait for completion of the country change event in
hdd reg set country API so that psoc operation for country set
gets completed only after the hdd country change work handle is
completed. So that idle shutdown does not proceed and change the
memory domain.
Also, add wait for completion of the country change event in
hdd psoc idle shutdown, so that it does not take the psoc

core/hdd/src/wlan_hdd_ioctl.c
@@ -2646,11 +2646,11 @@ static int drv_cmd_set_wmmps(struct wlan_hdd_link_info *link_info,
 	return hdd_wmmps_helper(link_info->adapter, command);
 }
 
-static inline int drv_cmd_country(struct wlan_hdd_link_info *link_info,
-				  struct hdd_context *hdd_ctx,
-				  uint8_t *command,
-				  uint8_t command_len,
-				  struct hdd_priv_data *priv_data)
+static inline int __drv_cmd_country(struct wlan_hdd_link_info *link_info,
+				    struct hdd_context *hdd_ctx,
+				    uint8_t *command,
+				    uint8_t command_len,
+				    struct hdd_priv_data *priv_data)
 {
 	char *country_code;
 
@@ -2675,6 +2675,26 @@ static inline int drv_cmd_country(struct wlan_hdd_link_info *link_info,
 		return -EINVAL;
 
 	return hdd_reg_set_country(hdd_ctx, country_code);
+}
+
+static inline int drv_cmd_country(struct wlan_hdd_link_info *link_info,
+				  struct hdd_context *hdd_ctx,
+				  uint8_t *command,
+				  uint8_t command_len,
+				  struct hdd_priv_data *priv_data)
+{
+	struct osif_psoc_sync *psoc_sync;
+	int errno;
+
+	errno = osif_psoc_sync_op_start(wiphy_dev(hdd_ctx->wiphy), &psoc_sync);
+	if (errno)
+		return errno;
+	errno = __drv_cmd_country(link_info, hdd_ctx, command, command_len,
+				  priv_data);
+
+	osif_psoc_sync_op_stop(psoc_sync);
+
+	return errno;
 }
 
 /**

CodeLinaro-mirror/external-wlan_qcacld-3.0

core/hdd/src/wlan_hdd_ioctl.c
@@ -2642,11 +2642,11 @@ static int drv_cmd_set_wmmps(struct hdd_adapter *adapter,
 	return hdd_wmmps_helper(adapter, command);
 }
 
-static inline int drv_cmd_country(struct hdd_adapter *adapter,
-				  struct hdd_context *hdd_ctx,
-				  uint8_t *command,
-				  uint8_t command_len,
-				  struct hdd_priv_data *priv_data)
+static inline int __drv_cmd_country(struct hdd_adapter *adapter,
+				    struct hdd_context *hdd_ctx,
+				    uint8_t *command,
+				    uint8_t command_len,
+				    struct hdd_priv_data *priv_data)
 {
 	char *country_code;
 
@@ -2671,6 +2671,26 @@ static inline int drv_cmd_country(struct hdd_adapter *adapter,
 		return -EINVAL;
 
 	return hdd_reg_set_country(hdd_ctx, country_code);
+}
+
+static inline int drv_cmd_country(struct hdd_adapter *adapter,
+				  struct hdd_context *hdd_ctx,
+				  uint8_t *command,
+				  uint8_t command_len,
+				  struct hdd_priv_data *priv_data)
+{
+	struct osif_psoc_sync *psoc_sync;
+	int errno;
+
+	errno = osif_psoc_sync_op_start(wiphy_dev(hdd_ctx->wiphy), &psoc_sync);
+	if (errno)
+		return errno;
+	errno = __drv_cmd_country(adapter, hdd_ctx, command, command_len,
+				  priv_data);
+
+	osif_psoc_sync_op_stop(psoc_sync);
+
+	return errno;
 }
 
 /**