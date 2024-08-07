46f54510bb73ba4dd3a861da5449a77b4d4f089a
Surya Prakash Sivaraj <quic_suryapra@quicinc.com>
2024-08-07 15:04:49 +0530

    qcacld-3.0: Add support for new ext connect interface
    The MRSNO support for connection would be received via new
    interface QCA_NL80211_VENDOR_SUBCMD_CONNECT_EXT.
    
    Add support to parse and save the config.
    
    Also, add support to send the RSN selector IE in the
    assoc request if the STA supports MRSNO feature.
    The RSN selector ID is:
    0 - RSNE association
    1 - RSNO1 association
    2 - RSNO2 association
    
    Change-Id: I463d26bfd29a6bb26f3cafafe5bc828b7b7cf419
    CRs-Fixed: 3890106

--- core/mac/src/pe/lim/lim_process_sme_req_messages.c
+++ core/mac/src/pe/lim/lim_process_sme_req_messages.c
@@ -4384,7 +4384,6 @@ lim_fill_session_params(struct mac_context *mac_ctx,
 		     session->ssId.length);
 
 	session->force_24ghz_in_ht20 = req->force_24ghz_in_ht20;
-	session->rsno_gen_used = req->rsno_gen_used;
 
 	status = lim_fill_pe_session(mac_ctx, session, bss_desc);
 	if (QDF_IS_STATUS_ERROR(status)) {
@@ -4467,6 +4466,14 @@ lim_fill_session_params(struct mac_context *mac_ctx,
 				      NULL, 0,
 				      mlme_priv->connect_info.ext_cap_ie,
 				      DOT11F_IE_EXTCAP_MAX_LEN);
+
+		if (wlan_get_rsn_sel_ie_from_ie_ptr(add_ie, add_ie_len) &&
+		    (req->entry->ie_list.wifi6_rsno ||
+		     req->entry->ie_list.wifi7_rsno))
+			session->rsno_gen_used = req->rsno_gen_used;
+		else
+			session->rsno_gen_used = 0;
+
 		qdf_mem_free(add_ie);
 
 		if (QDF_IS_STATUS_ERROR(status)) {
