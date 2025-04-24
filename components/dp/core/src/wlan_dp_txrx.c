arter97/caf_qcacld-3.0

qcacld-3.0: Fix VDEV NULL point dereference in DP TX
Currently in dp_get_transmit_mac_addr(), VDEV objmgr is referred without
incrementing the VDEV reference count, because of this there could be a
potential kernel NPE when VDEV deletion and dp_start_xmit() are in race.

Since taking VDEV references is discouraged in the per packet path due
to locking, a simple VDEV NULL check should solve the problem considering
the fact that DP VDEV object is already protected by
dp_intf::num_active_task.

Change-Id: I52229dc589feada1b1ffb261468915df88d1e486
CRs-Fixed: 3625809

components/dp/core/src/wlan_dp_txrx.c
@@ -421,6 +421,17 @@ void dp_get_transmit_mac_addr(struct wlan_dp_link *dp_link,
 	bool is_mc_bc_addr = false;
 	enum nan_datapath_state state;
 
+	/* Check for VDEV validity before accessing it. Since VDEV references
+	 * are not taken in the per packet path, there is a change for VDEV
+	 * getting deleted in a parallel context. Because DP VDEV object is
+	 * protected by dp_intf::num_active_task, the chance of VDEV object
+	 * getting deleted while executing dp_start_xmit() is sparse. So, a
+	 * simple VDEV NULL check should be sufficient to handle the case of
+	 * VDEV getting destroyed first followed by dp_start_xmit().
+	 */
+	if (!dp_link->vdev)
+		return;
+
 	switch (dp_intf->device_mode) {
 	case QDF_NDI_MODE:
 		state = wlan_nan_get_ndi_state(dp_link->vdev);

CodeLinaro-mirror/external-wlan_qcacld-3.0

components/dp/core/src/wlan_dp_txrx.c
@@ -420,6 +420,17 @@ void dp_get_transmit_mac_addr(struct wlan_dp_intf *dp_intf,
 	bool is_mc_bc_addr = false;
 	enum nan_datapath_state state;
 
+	/* Check for VDEV validity before accessing it. Since VDEV references
+	 * are not taken in the per packet path, there is a change for VDEV
+	 * getting deleted in a parallel context. Because DP VDEV object is
+	 * protected by dp_intf::num_active_task, the chance of VDEV object
+	 * getting deleted while executing dp_start_xmit() is sparse. So, a
+	 * simple VDEV NULL check should be sufficient to handle the case of
+	 * VDEV getting destroyed first followed by dp_start_xmit().
+	 */
+	if (!dp_intf->vdev)
+		return;
+
 	switch (dp_intf->device_mode) {
 	case QDF_NDI_MODE:
 		state = wlan_nan_get_ndi_state(dp_intf->vdev);