281155d4f75d4cb836b2d62d453dc4afee9b7075
Gururaj Pandurangi <panduran@qti.qualcomm.com>
2025-07-28 22:55:36 -0700

    qcacld-3.0: Disable bandwidth based subframe sizing for 512 block ack
    The block ack session is limited to 128 buffer size in firmware even
    though user configures the block ack buffer size greater than 128.
    The firmware limitation is due to bandwidth based subframe sizing
    for MLO connections.
    To fix above issue, configure the firmware to disable the bandwidth
    based subframe sizing to allow 512 block ack buffer size.
    
    CRs-Fixed: 4226351
    Change-Id: Id9e712bab3c2f353edef817ecdfdfb92f1b3b73c

--- core/sme/src/common/sme_api.c
+++ core/sme/src/common/sme_api.c
@@ -15797,6 +15797,7 @@ void sme_config_ba_mode_all_vdevs(mac_handle_t mac_handle, uint8_t val)
 	uint8_t vdev_id;
 	int ret_val = 0;
 	struct wlan_objmgr_vdev *vdev;
+	uint32_t arg[2];
 
 	for (vdev_id = 0; vdev_id < WLAN_MAX_VDEVS; vdev_id++) {
 		vdev = wlan_objmgr_get_vdev_by_id_from_pdev(mac->pdev,
@@ -15816,6 +15817,15 @@ void sme_config_ba_mode_all_vdevs(mac_handle_t mac_handle, uint8_t val)
 		else
 			sme_debug("vdev: %d ba mode: %d param id %d",
 				  vdev_id, val, wmi_vdev_param_set_ba_mode);
+		if (val == HDD_BA_MODE_512) {
+			arg[0] = 702;
+			arg[1] = 0;
+			ret_val = sme_send_unit_test_cmd(vdev_id,
+							 0x48, 2, arg);
+			if (QDF_IS_STATUS_ERROR(ret_val))
+				sme_err("Failed to disable bw based subframe sizing");
+		}
+
 	}
 }
 #endif
