c18450d2a8c946e0d9173c77971cb6d25c225b5f
Naga Maneendra Pokuri <npokuri@qti.qualcomm.com>
2025-08-21 22:24:34 -0700

    qcacld-3.0: Add Debug Support to Detect Long-Held Wake Locks
    Add wake_lifetime type to wake_lock struct to
    align with the Debug infra and init and deinit
    wake lock debug infra as part of driver load unload
    
    Change-Id: Ibb7a4884b863d6b782449e4e68e1e553141b9943
    CRs-Fixed: 4199134

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -259,6 +259,7 @@
 #include "wlan_hdd_tx_powerboost.h"
 #include "wifi_pos_pasn_api.h"
 #include "wlan_cp_stats_ucfg_api.h"
+#include "qdf_wakelock_debug.h"
 
 #ifdef MULTI_CLIENT_LL_SUPPORT
 #define WLAM_WLM_HOST_DRIVER_PORT_ID 0xFFFFFF
@@ -12662,6 +12663,8 @@ void hdd_wlan_exit(struct hdd_context *hdd_ctx)
 
 	unregister_netdevice_notifier(&hdd_netdev_notifier);
 
+	qdf_dbg_wake_lock_tracker_deinit();
+
 	qdf_dp_trace_deinit();
 
 	hdd_wlan_stop_modules(hdd_ctx, false);
@@ -12787,7 +12790,8 @@ struct hdd_adapter *hdd_get_first_valid_adapter(struct hdd_context *hdd_ctx)
 /* wake lock APIs for HDD */
 void hdd_prevent_suspend(uint32_t reason)
 {
-	qdf_wake_lock_acquire(&wlan_wake_lock, reason);
+	qdf_wake_lock_acquire(&wlan_wake_lock, reason,
+			      QDF_WAKE_TIME_DEFINED);
 }
 
 void hdd_allow_suspend(uint32_t reason)
@@ -21434,6 +21438,7 @@ static QDF_STATUS hdd_qdf_init(void)
 	qdf_delayed_work_feature_init();
 	qdf_periodic_work_feature_init();
 	qdf_wake_lock_feature_init();
+	qdf_dbg_wake_lock_tracker_init();
 	qdf_mc_timer_manager_init();
 	qdf_event_list_init();
 
@@ -21469,6 +21474,7 @@ talloc_deinit:
 event_deinit:
 	qdf_event_list_destroy();
 	qdf_mc_timer_manager_exit();
+	qdf_dbg_wake_lock_tracker_deinit();
 	qdf_wake_lock_feature_deinit();
 	qdf_periodic_work_feature_deinit();
 	qdf_delayed_work_feature_deinit();
@@ -21495,6 +21501,7 @@ static void hdd_qdf_deinit(void)
 	qdf_talloc_feature_deinit();
 	qdf_event_list_destroy();
 	qdf_mc_timer_manager_exit();
+	qdf_dbg_wake_lock_tracker_deinit();
 	qdf_wake_lock_feature_deinit();
 	qdf_periodic_work_feature_deinit();
 	qdf_delayed_work_feature_deinit();
@@ -21746,7 +21753,8 @@ static int __hdd_driver_mode_change(struct hdd_context *hdd_ctx,
 
 		hdd_info("Acquire wakelock for monitor mode");
 		qdf_wake_lock_acquire(&hdd_ctx->monitor_mode_wakelock,
-				      WIFI_POWER_EVENT_WAKELOCK_MONITOR_MODE);
+				      WIFI_POWER_EVENT_WAKELOCK_MONITOR_MODE,
+				      QDF_WAKE_TIME_UNDEFINED);
 	}
 
 	/* con_mode is a global module parameter */
