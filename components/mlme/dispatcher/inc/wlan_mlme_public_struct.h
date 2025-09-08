b2c98fb064f62a5a6b274c9b9bf8319ad9aa6ca2
Akshay Mohite <mohite@qti.qualcomm.com>
2025-09-08 09:38:57 +0530

    qcacld-3.0: Add ini config for STA indoor channel peer SCC
    Add new ini configuration for setting, peer to peer connections
    on STA connected indoor channel SCC support.
    
    CRs-Fixed: 4297915
    Change-Id: Ie10f71d3818dbc3b7c0fa9c4a1b93bd133d52d00

--- components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
+++ components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
@@ -2865,6 +2865,8 @@ enum mlme_reg_srd_master_modes {
  * @coex_unsafe_chan_reg_disable: To disable reg channels for received coex
  * unsafe channels list
  * @enable_c2c_support: Enable C2C support flag
+ * @sta_indoor_ch_peer_scc : Support STA connected indoor channel for
+ * peer-to-peer connections and from SCC.
  */
 struct wlan_mlme_reg {
 	uint32_t self_gen_frm_pwr;
@@ -2899,6 +2901,7 @@ struct wlan_mlme_reg {
 #ifdef CONFIG_BAND_6GHZ
 	bool enable_c2c_support;
 #endif
+	bool sta_indoor_ch_peer_scc;
 };
 
 #define IOT_AGGR_INFO_MAX_NUM 32
