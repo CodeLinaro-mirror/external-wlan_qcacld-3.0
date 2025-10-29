05676957f36ff5eec4d54da51256b7b83fad4350
Sanskar Jain <sansjain@qti.qualcomm.com>
2025-10-29 18:01:03 +0530

    qcacld-3.0: Add new INI to send TXOP duration to firmware
    Add new INI for configuring TXOP limit
    "gEdcaTxopLimit" -
    	TXOP duration limit values in milliseconds.
    
    This INI will be sent to Firmware as part of vdev create params
    once VDEV is created.
    
    Firmware use the non-zero value to override current burst TXOP and
    force SIFS burst on the vdev.
    
    Change-Id: I0312eb593ca913eb802873c40b9a4aa86543f2ea
    CRs-Fixed: 4336019

--- components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
+++ components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
@@ -1534,6 +1534,7 @@ struct wlan_mlme_aux_dev_caps {
  * TX/RX bytes.
  * @sap_traffic_monitoring_time_s: Duration of traffic monitoring
  * in unit of sec
+ * @edca_txop_limit: EDCA TXOP limit in milliseconds.
  */
 struct wlan_mlme_generic {
 	uint32_t band_capability;
@@ -1613,6 +1614,7 @@ struct wlan_mlme_generic {
 	uint32_t sap_perf_data_threshold;
 	uint32_t sap_traffic_monitoring_time_s;
 #endif
+	uint32_t edca_txop_limit;
 };
 
 /**
