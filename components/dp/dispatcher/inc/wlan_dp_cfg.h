arter97-mirror/caf_qcacld-3.0

qcacld-3.0: Added HAPS feature to avoid unwanted FW wakeups
Host Aware Power Save (HAPS) is a feature that, when enables,
allows the firmware to update the host on its power states.
The host then checks these states before deciding whether to
coalesce packets. This process helps prevent unnecessary wakeups
and also starts QTimer synchronization if enabled.

Change-Id: Id6c42abbc6ab33ba4098f866119294f6ad818148
CRs-Fixed: 4184956

components/dp/dispatcher/inc/wlan_dp_cfg.h
@@ -1300,6 +1300,43 @@
 #define CFG_DP_FISA
 #endif
 
+#ifdef WLAN_HAPS_ENABLE
+#define WLAN_CFG_HAPS_CONFIG         (0)
+#define WLAN_CFG_HAPS_CONFIG_MIN     (0)
+#define WLAN_CFG_HAPS_CONFIG_MAX     (0xFFFFFFFF)
+
+/*
+ * <ini>
+ * dp_haps_config - Power save configuration for HAPS
+ *
+ * @min: 0x0
+ * @max: 0xffffffff
+ * @default: 0x0
+ *
+ * |31                         2|        1          |     0      |
+ * +----------------------------+-------------------+------------+
+ * |           RSVD             |       sync        |    E/D     |
+ * +----------------------------+-------------------+------------+
+ *
+ * bit 0    : Enable/Disable haps feature
+ * bit 1    : Sync and update qtime cnss timestamp
+ * bit 2-31 : Reserved for future usage
+ *
+ * </ini>
+ */
+#define CFG_HAPS_CONFIG \
+	CFG_INI_UINT("dp_haps_config", \
+		     WLAN_CFG_HAPS_CONFIG_MIN, \
+		     WLAN_CFG_HAPS_CONFIG_MAX, \
+		     WLAN_CFG_HAPS_CONFIG, \
+		     CFG_VALUE_OR_DEFAULT, \
+		     "HAPS flags for power save")
+
+#define CFG_DP_HAPS CFG(CFG_HAPS_CONFIG)
+#else
+#define CFG_DP_HAPS
+#endif
+
 /*TODO Flow control part to be moved to DP later*/
 
 #ifdef WLAN_FEATURE_DP_BUS_BANDWIDTH
@@ -1635,6 +1672,7 @@
 	CFG_DP_DYNAMIC_RESOURCE_MGMT \
 	CFG(CFG_DP_IPA_DEBUG_ENABLE) \
 	CFG(CFG_DP_IRQ_AFFINITY_MASK) \
-	CFG(CFG_DP_RX_THREAD_AFFINITY_MASK)
+	CFG(CFG_DP_RX_THREAD_AFFINITY_MASK) \
+	CFG_DP_HAPS
 
 #endif /* WLAN_DP_CFG_H__ */

CodeLinaro-mirror/external-wlan_qcacld-3.0

components/dp/dispatcher/inc/wlan_dp_cfg.h
@@ -1300,6 +1300,43 @@
 #define CFG_DP_FISA
 #endif
 
+#ifdef WLAN_HAPS_ENABLE
+#define WLAN_CFG_HAPS_CONFIG         (0)
+#define WLAN_CFG_HAPS_CONFIG_MIN     (0)
+#define WLAN_CFG_HAPS_CONFIG_MAX     (0xFFFFFFFF)
+
+/*
+ * <ini>
+ * dp_haps_config - Power save configuration for HAPS
+ *
+ * @min: 0x0
+ * @max: 0xffffffff
+ * @default: 0x0
+ *
+ * |31                         2|        1          |     0      |
+ * +----------------------------+-------------------+------------+
+ * |           RSVD             |       sync        |    E/D     |
+ * +----------------------------+-------------------+------------+
+ *
+ * bit 0    : Enable/Disable haps feature
+ * bit 1    : Sync and update qtime cnss timestamp
+ * bit 2-31 : Reserved for future usage
+ *
+ * </ini>
+ */
+#define CFG_HAPS_CONFIG \
+	CFG_INI_UINT("dp_haps_config", \
+		     WLAN_CFG_HAPS_CONFIG_MIN, \
+		     WLAN_CFG_HAPS_CONFIG_MAX, \
+		     WLAN_CFG_HAPS_CONFIG, \
+		     CFG_VALUE_OR_DEFAULT, \
+		     "HAPS flags for power save")
+
+#define CFG_DP_HAPS CFG(CFG_HAPS_CONFIG)
+#else
+#define CFG_DP_HAPS
+#endif
+
 /*TODO Flow control part to be moved to DP later*/
 
 #ifdef WLAN_FEATURE_DP_BUS_BANDWIDTH
@@ -1583,6 +1620,7 @@
 	CFG_DP_STC \
 	CFG_DP_DYNAMIC_RESOURCE_MGMT \
 	CFG(CFG_DP_IPA_DEBUG_ENABLE) \
-	CFG(CFG_DP_IRQ_AFFINITY_MASK)
+	CFG(CFG_DP_IRQ_AFFINITY_MASK) \
+	CFG_DP_HAPS
 
 #endif /* WLAN_DP_CFG_H__ */