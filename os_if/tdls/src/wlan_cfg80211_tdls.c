8c5ff05df6d9c4122aa1f967cbabe5b7c0e8d073
jincheng <jincheng@qti.qualcomm.com>
2026-07-17 22:43:39 +0800

    Revert "qcacld-3.0: guard tdls station_parameters struct access by cfg80211 macros"
    CR-Fixed: 4585786
    Change-Id: Ic0a8e8c9acf56f399b9599873799fbbe60437094

--- os_if/tdls/src/wlan_cfg80211_tdls.c
+++ os_if/tdls/src/wlan_cfg80211_tdls.c
@@ -276,8 +276,6 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 		qdf_mem_copy(req_info->extn_capability, params->ext_capab,
 			     sizeof(req_info->extn_capability));
 
-#if defined(CFG80211_LINK_STA_PARAMS_PRESENT) || \
-    defined(CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT)
 	if (params->link_sta_params.ht_capa) {
 		req_info->htcap_present = 1;
 		qdf_mem_copy(&req_info->ht_cap, params->link_sta_params.ht_capa,
@@ -285,15 +283,6 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 	}
 
 	req_info->supported_rates_len = params->link_sta_params.supported_rates_len;
-#else
-	if (params->ht_capa) {
-		req_info->htcap_present = 1;
-		qdf_mem_copy(&req_info->ht_cap, params->ht_capa,
-			     sizeof(struct htcap_cmn_ie));
-	}
-
-	req_info->supported_rates_len = params->supported_rates_len;
-#endif
 
 	/* Note : The Maximum sizeof supported_rates sent by the Supplicant is
 	 * 32. The supported_rates array , for all the structures propogating
@@ -371,17 +360,6 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 		qdf_mem_copy(req_info->extn_capability, params->ext_capab,
 			     sizeof(req_info->extn_capability));
 
-#if defined(CFG80211_LINK_STA_PARAMS_PRESENT) || \
-    defined(CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT)
-	if (params->link_sta_params.ht_capa) {
-		req_info->htcap_present = 1;
-		qdf_mem_copy(&req_info->ht_cap, params->link_sta_params.ht_capa,
-			     sizeof(struct htcap_cmn_ie));
-	}
-
-	req_info->supported_rates_len = params->link_sta_params.supported_rates_len;
-#else
-
 	if (params->ht_capa) {
 		req_info->htcap_present = 1;
 		qdf_mem_copy(&req_info->ht_cap, params->ht_capa,
@@ -389,7 +367,6 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 	}
 
 	req_info->supported_rates_len = params->supported_rates_len;
-#endif
 
 	/* Note : The Maximum sizeof supported_rates sent by the Supplicant is
 	 * 32. The supported_rates array , for all the structures propogating
@@ -402,30 +379,6 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 	if (req_info->supported_rates_len > WLAN_MAC_MAX_SUPP_RATES)
 		req_info->supported_rates_len = WLAN_MAC_MAX_SUPP_RATES;
 
-#if defined(CFG80211_LINK_STA_PARAMS_PRESENT) || \
-    defined(CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT)
-	if (req_info->supported_rates_len) {
-		qdf_mem_copy(req_info->supported_rates,
-			     params->link_sta_params.supported_rates,
-			     req_info->supported_rates_len);
-		osif_debug("Supported Rates with Length %d",
-			   req_info->supported_rates_len);
-
-		for (i = 0; i < req_info->supported_rates_len; i++)
-			osif_debug("[%d]: %0x", i,
-				   req_info->supported_rates[i]);
-	}
-
-	if (params->link_sta_params.vht_capa) {
-		req_info->vhtcap_present = 1;
-		qdf_mem_copy(&req_info->vht_cap, params->link_sta_params.vht_capa,
-			     sizeof(struct vhtcap));
-	}
-
-	if (params->link_sta_params.ht_capa || params->link_sta_params.vht_capa ||
-	    (params->sta_flags_set & BIT(NL80211_STA_FLAG_WME)))
-		req_info->is_qos_wmm_sta = true;
-#else
 	if (req_info->supported_rates_len) {
 		qdf_mem_copy(req_info->supported_rates,
 			     params->supported_rates,
@@ -438,16 +391,15 @@ wlan_cfg80211_tdls_extract_params(struct tdls_update_peer_params *req_info,
 				   req_info->supported_rates[i]);
 	}
 
-	if (params->link_sta_params.vht_capa) {
+	if (params->vht_capa) {
 		req_info->vhtcap_present = 1;
-		qdf_mem_copy(&req_info->vht_cap, params->link_sta_params.vht_capa,
+		qdf_mem_copy(&req_info->vht_cap, params->vht_capa,
 			     sizeof(struct vhtcap));
 	}
 
-	if (params->link_sta_params.ht_capa || params->link_sta_params.vht_capa ||
+	if (params->ht_capa || params->vht_capa ||
 	    (params->sta_flags_set & BIT(NL80211_STA_FLAG_WME)))
 		req_info->is_qos_wmm_sta = true;
-#endif
 	if (params->sta_flags_set & BIT(NL80211_STA_FLAG_MFP)) {
 		osif_debug("TDLS peer pmf capable");
 		req_info->is_pmf = 1;
