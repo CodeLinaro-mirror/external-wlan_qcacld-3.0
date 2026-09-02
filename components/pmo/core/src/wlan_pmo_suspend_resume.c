a3343aca50e2c7b1b3a4958dd649ab9bcfbaa8a1
Zhiwei Yang <zhiwyang@qti.qualcomm.com>
2026-09-02 17:41:03 +0800

    qcacld-3.0: Workaround to avoid FW crash during suspend
    Sequence of crash process:
    1. FW sends the WOW_TRACE_WOW_SUSPEND_ACK_SENT packet.
    2. The packet completion IRQ is received by the core, which
       invokes theHIF_TRACE_ISR_CALLBACK_TX FW callback.
    3. As soon as the host receives the WOW_TRACE_WOW_SUSPEND_ACK_SENT
       packet, it initiates device suspend and suspends the SDIO client.
    4. Approximately 2 ms later, FW invokes the HIF_TRACE_DSR_HANDLER_TX
       callback.
    5. By this time, the SDIO client has already been suspended by
       the host. The HIF_TRACE_DSR_HANDLER_TX calls the
       sdioc_dsr_buff_dequeue core API while the SDIO client is
       suspended, resulting in a crash.
    
    Root Cause:
    There is a timing gap between the HIF_TRACE_ISR_CALLBACK_TX
    and HIF_TRACE_DSR_HANDLER_TX callbacks. During this interval,
    the host suspends the SDIO client, but FW continues processing
    and invokes core SDIO APIs, leading to a crash.
    
    This change is a workaround which was recommended by FW side. Host
    wait for 10 ms after receiving the WOW_TRACE_WOW_SUSPEND_ACK_SENT
    packet before initiating SDIO suspend. This delay allows FW to
    complete all the processing, including the HIF_TRACE_DSR_HANDLER_TX
    callback, thereby preventing access to SDIO core APIs after the SDIO
    client has been suspended.
    
    Change-Id: Ibdf1aba9bcd0585f99373af61a0fc06818962049

--- components/pmo/core/src/wlan_pmo_suspend_resume.c
+++ components/pmo/core/src/wlan_pmo_suspend_resume.c
@@ -915,6 +915,7 @@ pmo_core_enable_wow_in_fw(struct wlan_objmgr_psoc *psoc,
 		htc_dump_counter_info(pmo_core_psoc_get_htc_handle(psoc));
 		qdf_trigger_self_recovery(psoc, QDF_SUSPEND_NO_CREDIT);
 	}
+	msleep(10);
 	pmo_debug("WOW enabled successfully in fw: credits:%d pending_cmds: %d",
 		host_credits, wmi_pending_cmds);
 
